eb74c2eb24d7612a86479f050ecab5a1906e77f4
James Wood <james@curious12.com>
2021-12-04 01:24:16 +0000

    Setting up Ava Sherry

---
+++ src/js/barba.js
@@ -0,0 +1,56 @@
+
+import barba from '@barba/core';
+import barbaCSS from '@barba/css';
+
+export default () => {
+
+    const bodyTag = document.querySelector('body');
+
+    barba.use(barbaCSS);
+
+    barba.init({
+        transitions: [
+            {
+                name: "fade",
+                once: () => {
+
+                },
+                beforeEnter: ({ current, next, trigger }) => {
+
+                    const headerLinks = document.querySelectorAll('header a');
+                    const href = next.url.path
+
+                    headerLinks.forEach((link) => {
+                        if (link.getAttribute('href') === href) {
+                            link.classList.add('selected');
+                        } else {
+                            link.classList.remove('selected');
+                        }                
+                    });
+
+                    window.scrollTo({
+                        top: 0,
+                        behavior: "smooth",
+                    });
+
+                },
+            }
+        ],
+
+        views: [
+            {
+                namespace: "feed",
+                beforeEnter: () => {
+                    bodyTag.classList.add('feed');
+                },
+                beforeLeave: () => {
+                    bodyTag.classList.remove('feed');
+                }
+
+            }
+        ],
+
+    });
+
+
+}
